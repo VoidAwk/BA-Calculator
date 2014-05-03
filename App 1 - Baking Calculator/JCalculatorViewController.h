183af82b1e9d8a9916b1e47b375332210e4a7983
Joe Ilagan <joeilagan@Joes-MacBook-Pro-2.local>
2014-05-03 18:06:03 +0800

    initial commit

---
+++ App 1 - Baking Calculator/JCalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCalculatorViewController.h
+//  App 1 - Baking Calculator
+//
+//  Created by Joe Ilagan on 5/3/14.
+//  Copyright (c) 2014 Void Awk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCalculatorViewController : UIViewController
+
+@end
